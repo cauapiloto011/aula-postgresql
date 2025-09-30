8b27bb9929635208386f41c5de0e6bf38c1779c6
Cauã Theodoro <caua82477@gmail.com>
2025-09-30 16:08:18 -0300

    Estrutura de pasta do projeto

---
+++ db.py
@@ -0,0 +1,16 @@
+import psycopg2
+# pip install psycopg2
+# pip install dotenv
+from dotenv import load_dotenv
+import os
+
+#Carregar as variáveis do .env
+load_dotenv()
+
+params = {
+    "dbname": os.getenv("DB_NAME"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "host": os.getenv("DB_HOST"),
+    "port": os.getenv("DB_PORT"),
+}
