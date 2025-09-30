11e51e2249a96b8402788c4968b1f3cf0dcb817f
Cauã Theodoro <caua82477@gmail.com>
2025-09-30 16:43:07 -0300

    add função de conectar

--- db.py
+++ db.py
@@ -1,4 +1,4 @@
-import psycopg2
+import psycopg2 as pg
 # pip install psycopg2
 # pip install dotenv
 from dotenv import load_dotenv
@@ -14,3 +14,12 @@ params = {
     "host": os.getenv("DB_HOST"),
     "port": os.getenv("DB_PORT"),
 }
+
+def conectar():
+    try:
+        conexao = pg.connect(**params)
+        cursor = conexao.cursor()
+        return conexao, cursor
+    except Exception as erro:
+        print(f"Erro de conexão {erro}")
+        return None, None
